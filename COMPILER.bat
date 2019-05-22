605af6b5e62293ebae046eba93f5edaa85ac869a
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2019-05-22 18:24:26 +0200

    Signed software, nice

--- COMPILER.bat
+++ COMPILER.bat
@@ -7,3 +7,6 @@ pyinstaller index.py --onefile --icon=logo.ico --name %filename%.exe
 
 :: Copy compiled file to root
 copy dist\%filename%.exe .
+
+:: Sign that nice software
+bin\signtool.exe sign /f "%userprofile%\.ssl\sign.pfx" /t http://timestamp.verisign.com/scripts/timstamp.dll %filename%.exe
