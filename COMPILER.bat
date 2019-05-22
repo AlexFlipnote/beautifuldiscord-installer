dd611a58bf0c32a868ed51b2683c8e43026737c6
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2019-05-22 19:25:05 +0200

    Fuck signing..

--- COMPILER.bat
+++ COMPILER.bat
@@ -1,5 +1,5 @@
 @echo off
-
+:: Variables
 set filename="beautifuldiscord"
 
 :: Make it compile beautifuldiscord to an executable file
@@ -7,6 +7,3 @@ pyinstaller index.py --onefile --icon=logo.ico --name %filename%.exe
 
 :: Copy compiled file to root
 copy dist\%filename%.exe .
-
-:: Sign that nice software
-bin\signtool.exe sign /f "%userprofile%\.ssl\sign.pfx" /t http://timestamp.verisign.com/scripts/timstamp.dll %filename%.exe
