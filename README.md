5a1102a0e57106835a15b41d17f0eaa39298474c
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2019-05-22 16:54:23 +0200

    More options

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # beautifuldiscord-installer
 An easier alternative of installing BeautifulDiscord with Discord_Theme project
+
+### Sources
+BeautifulDiscord is created by [leovoel](https://github.com/leovoel/BeautifulDiscord)
+and licensed under [MIT License](https://github.com/leovoel/BeautifulDiscord/blob/master/LICENSE)
--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import sys
 
 from utils import app as beautifuldiscord
 
@@ -9,11 +10,12 @@ class Installer:
         self.line = "----------------------------------------"
         self.name = "BeautifulDiscord"
         self.theme_url = "https://raw.githubusercontent.com/AlexFlipnote/Discord_Theme/master/autotheme.css"
-        self.choices = [1, 2, 3]
+        self.choices = [1, 2, 3, 4, 5]
 
         saveDir = os.path.expanduser("~/Documents/DiscordTheme/")
         if not os.path.exists(saveDir):
             os.makedirs(saveDir)
+            print("Created a new folder in your Documents called: DiscordTheme")
 
         self.location = saveDir
         self.filename = "autotheme.css"
@@ -29,15 +31,21 @@ class Installer:
         self.printer(
             "Welcome to BeautifulDiscord install tool",
             self.line,
-            "1 - Download theme and install BeautifulDiscord",
-            "2 - Enable theme after Discord update",
-            "3 - Uninstall theme from Discord",
+            "1 - Install BeautifulDiscord with pre-made theme (AlexFlipnote/Discord_Theme)",
+            "2 - Install BeautifulDiscord from scratch",
+            "3 - Enable theme after Discord update (Sometimes Discord disables the theme, idk)",
+            "4 - Uninstall theme from Discord",
+            "5 - Close this window",
             self.line,
             "Enter the number choice you want and press enter"
         )
 
-    def write_theme_file(self):
-        file = self.get_file(self.theme_url)
+    def write_theme_file(self, download_theme: bool = True):
+        if download_theme:
+            file = self.get_file(self.theme_url)
+        else:
+            file = b"/* Add your own CSS inside here */"
+
         if not file:
             print("Failed to inject file, got nothing from downloader...")
 
@@ -45,8 +53,8 @@ class Installer:
             with open(self.location + self.filename, "wb") as f:
                 f.write(file)
             return True
-        except Exception:
-            print("I have no clue what happened, but I failed to make a theme file...")
+        except Exception as e:
+            print(f"I have no clue what happened, but I failed to make a theme file...\nDEBUG: {e}")
             return False
 
     def inject_theme(self):
@@ -89,21 +97,30 @@ class Installer:
     def main_script(self):
         while True:
             self.print_options()
-            choice = self.choose()
-            if choice:
+            userchoice = self.choose()
+            if userchoice:
                 break
 
-        if choice == 1:
+        if userchoice == 1:
             self.write_theme_file()
             self.inject_theme()
-        if choice == 2:
+        if userchoice == 2:
+            self.write_theme_file(download_theme=False)
+            self.inject_theme()
+        if userchoice == 3:
             inject = self.inject_theme()
             if not inject:
-                print("Could not find the CSS file to inject, please try option 1 instead.")
-        if choice == 3:
+                print("Could not find the CSS file to inject, please try option 1 or 2 instead.")
+        if userchoice == 4:
             beautifuldiscord.main("--revert")
+        if userchoice == 5:
+            sys.exit(0)
 
         input("\nYou can now close this window")
 
 
-Installer()
+if __name__ == '__main__':
+    try:
+        Installer()
+    except KeyboardInterrupt:
+        print("Stopping process...")
